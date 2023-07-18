83444a1e33dc0818f031e647499e14719bb3dac0
ForeverExiled <foreverexiled23@gmail.com>
2023-07-18 20:59:05 +0500

    fix: delete unneeded code&class

--- local/api/classes/Catalog.php
+++ local/api/classes/Catalog.php
@@ -5,9 +5,6 @@ namespace Legacy\API;
 use Bitrix\Main\Loader;
 use Bitrix\Iblock\PropertyTable;
 use Bitrix\Iblock\SectionTable;
-use Bitrix\Iblock\ElementPropertyTable;
-use Legacy\Catalog\CatalogTable;
-use Legacy\Catalog\FilterTable;
 use Legacy\Catalog\ProductElementTable;
 use Legacy\General\CaseChangerHelper;
 use Legacy\General\Constants;
@@ -51,68 +48,6 @@ class Catalog
                     'id' => $res['ID'],
                     'name' => $res['NAME'],
                     'code' => $res['CODE'],
-                ];
-            }
-        }
-
-        return $result;
-    }
-
-    public static function getFilter($arRequest, $properties)
-    {
-        $result = [
-            'categories' => [
-                'name' => 'Категория',
-                'items' => self::getCategories()
-            ],
-        ];
-
-        if (Loader::includeModule('iblock') && Loader::includeModule('catalog')) {
-            $category = $arRequest['category'] ?? '';
-            $filter = $arRequest['filter'] ?? [];
-            if (count($filter) == 1) {
-                foreach ($properties as $pcode => $property) {
-                    if (mb_strtolower($property['CODE']) == mb_strtolower((key($filter)))) {
-                        foreach ($property['VALUES'] as $key => $value) {
-                            $properties[$pcode]['VALUES'][$key]['is_available'] = true;
-                        }
-                        break;
-                    }
-                }
-            }
-
-            $q = FilterTable::query()->withDefault($properties)->withFromCategory($category)->withFilter($filter);
-            $db = $q->exec();
-            while ($arr = $db->fetch()) {
-                foreach ($arr as $column => $value) {
-                    if (mb_strpos($column, 'P_') === 0) {
-                        $code = mb_substr($column, 2);
-                        foreach ($properties as $pcode => $property) {
-                            if ($pcode == $code) {
-                                $properties[$pcode]['VALUES'][$value]['is_available'] = true;
-                            }
-                        }
-                    }
-                }
-            }
-
-            foreach ($properties as $pid => $property) {
-                $isEmpty = true;
-                foreach($property['VALUES'] as $code => $value) {
-                    $isEmpty = !$value;
-                    if (!$isEmpty) {
-                        break;
-                    }
-                }
-                if ($isEmpty) {
-                    unset($properties[$pid]);
-                }
-            }
-
-            foreach ($properties as $pid => $property) {
-                $result[mb_strtolower($property['CODE'])] = [
-                    'name' => $property['NAME'],
-                    'items' => $property['VALUES'],
                 ];
             }
         }
